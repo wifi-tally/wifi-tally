85a3c378051d574b76ed09e5bb9a2fc7dabf62c2
Christian Zenker <christian.zenker@aoe.com>
2020-03-15 14:37:51 +0100

    log the boot reason of the tally to detect software errors

--- tally/src/my-app.lua
+++ tally/src/my-app.lua
@@ -6,5 +6,37 @@ require('my-led')
 require('my-tally')
 require('my-wifi')
 
+local bootreasons = {
+    POWER_ON = 0,
+    HW_WATCHDOG_RESET = 1,
+    EXCEPTION_RESET = 2,
+    SW_WATCHDOG_RESET = 3,
+    SOFTWARE_RESTART = 4,
+    WAKE_FROM_DEEP_SLEEP = 5,
+    EXTERNAL_RESET = 6,
+}
+
+local _, bootreason, exccause, epc1, epc2, epc3, excvaddr, depc = node.bootreason()
+local humanReadable = bootreason
+for key, val in pairs(bootreasons) do
+    if val == bootreason then
+        humanReadable = key
+        break
+    end
+end
+
+local fn = MyLog.info
+if bootreason == bootreasons.HW_WATCHDOG_RESET or bootreason == bootreasons.SW_WATCHDOG_RESET then
+    fn = MyLog.error
+end
+
+fn("booted because of " .. humanReadable)
+
+for _, thing in pairs({exccause, epc1, epc2, epc3, excvaddr, depc}) do
+    if type(thing) == "string" then
+        MyLog.error(thing)
+    end
+end
+
 MyLed.initial()
 MyWifi.connect()
