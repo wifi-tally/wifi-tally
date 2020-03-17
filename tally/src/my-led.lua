58e4e89b5d47c2d7e2aa7cf0b88804ad70c2752c
Christian Zenker <dev@xopn.de>
2020-03-17 21:20:35 +0100

    add the possibility to have main light (for the speaker) and a light for the operator #2

--- tally/src/my-led.lua
+++ tally/src/my-led.lua
@@ -1,17 +1,35 @@
-local pinG, pinR, pinB = 7, 6, 5
+-- pins used for the operators light
+local pinOpG, pinOpR, pinOpB = 7, 6, 5
+
+-- pins used for the main light
+local pinMainG, pinMainR, pinMainB = 4, 3, 2
 
 -- the timer used for the status LED that signalizes network errors
 local timer = tmr.create()
 
-pwm2.setup_pin_hz(pinR, 1000, 100, 0)
-pwm2.setup_pin_hz(pinG, 1000, 100, 0)
-pwm2.setup_pin_hz(pinB, 1000, 100, 0)
+pwm2.setup_pin_hz(pinOpR, 1000, 100, 100)
+pwm2.setup_pin_hz(pinOpG, 1000, 100, 100)
+pwm2.setup_pin_hz(pinOpB, 1000, 100, 100)
+pwm2.setup_pin_hz(pinMainR, 1000, 100, 100)
+pwm2.setup_pin_hz(pinMainG, 1000, 100, 100)
+pwm2.setup_pin_hz(pinMainB, 1000, 100, 100)
 pwm2.start()
+
+local colors = {
+    RED = {true, false, false},
+    GREEN = {false, true, false},
+    BLUE = {false, false, true},
+    WHITE = {true, true, true},
+    BLACK = {false, false, false},
+}
 
 local nextId = 0
 local currentId = nil
 
-local flashPattern = function(pattern, colorR, colorG, colorB, seconds)
+local flashPattern = function(pattern, color, seconds)
+    local colorR, colorG, colorB = color[1], color[2], color[3]
+    local showOnMain = color ~= colors.BLUE
+
     seconds = seconds or 1
     local id = nextId
     nextId = nextId + 1
@@ -23,13 +41,19 @@ local flashPattern = function(pattern, colorR, colorG, colorB, seconds)
         local isOn = pattern:sub(idx+1, idx+1) ~= " "
 
         if isOn then
-            pwm2.set_duty(pinR, colorR and 0 or 100)
-            pwm2.set_duty(pinG, colorG and 0 or 100)
-            pwm2.set_duty(pinB, colorB and 0 or 100)
+            pwm2.set_duty(pinOpR, colorR and 0 or 100)
+            pwm2.set_duty(pinOpG, colorG and 0 or 100)
+            pwm2.set_duty(pinOpB, colorB and 0 or 100)
+            pwm2.set_duty(pinMainR, colorR and showOnMain and 0 or 100)
+            pwm2.set_duty(pinMainG, colorG and showOnMain and 0 or 100)
+            pwm2.set_duty(pinMainB, colorB and showOnMain and 0 or 100)
         else
-            pwm2.set_duty(pinR, colorR and 70 or 100)
-            pwm2.set_duty(pinG, colorG and 70 or 100)
-            pwm2.set_duty(pinB, colorB and 70 or 100)
+            pwm2.set_duty(pinOpR, colorR and 70 or 100)
+            pwm2.set_duty(pinOpG, colorG and 70 or 100)
+            pwm2.set_duty(pinOpB, colorB and 70 or 100)
+            pwm2.set_duty(pinMainR, colorR and showOnMain and 70 or 100)
+            pwm2.set_duty(pinMainG, colorG and showOnMain and 70 or 100)
+            pwm2.set_duty(pinMainB, colorB and showOnMain and 70 or 100)
         end
     end
 
@@ -45,15 +69,13 @@ end
 
 _G.MyLed = {
     -- signal that nothing is being done
-    initial = flashPattern("O", false, false, true),
-    waitForWifiConnection = flashPattern("O ", false, false, true),
-    waitForWifiIp = flashPattern("O O ", false, false, true),
-    waitForServerConnection = flashPattern("O O   ", false, false, true),
-    onPreview = flashPattern("O", false, true, false),
-    onAir = flashPattern("O", true, false, false),
-    onRelease = flashPattern("O", false, false, false),
-    onUnknown = flashPattern("O       ", false, false, true, 2),
-    onHighlight = flashPattern("O O O O ", true, true, true),
-    cp = flashPattern("O ", true, false, true),
-    cpConnected = flashPattern("O", true, false, true),
+    initial = flashPattern("O", colors.BLUE),
+    waitForWifiConnection = flashPattern("O ", colors.BLUE),
+    waitForWifiIp = flashPattern("O O ", colors.BLUE),
+    waitForServerConnection = flashPattern("O O   ", colors.BLUE),
+    onPreview = flashPattern("O", colors.GREEN),
+    onAir = flashPattern("O", colors.RED),
+    onRelease = flashPattern("O", colors.BLACK),
+    onUnknown = flashPattern("O       ", colors.BLUE, 2),
+    onHighlight = flashPattern("O O O O ", colors.WHITE),
 }
