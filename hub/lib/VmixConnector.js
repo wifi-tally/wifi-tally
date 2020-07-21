30e385f5f1574fc259558dee3db11bf7e4151639
Christian Zenker <dev@xopn.de>
2020-07-21 22:33:08 +0200

    better error handling for vMix #12

--- hub/lib/VmixConnector.js
+++ hub/lib/VmixConnector.js
@@ -13,21 +13,48 @@ class VmixConnector {
     connect() {
         const client = new net.Socket()
         this.client = client
-        client.connect(this.port, this.ip, () => {
-            console.log('Connected')
+
+        const connectClient = () => {
+            this.wasHelloReceived = false
+            this.wasSubcribeOkReceived = false
+            client.connect(this.port, this.ip)
+        }
+
+        connectClient()
+
+        client.on("connect", () => {
+            console.log('Connected to vMix')
+            this.emitter.emit('mixer.connected')
+        })
+
+        client.on("ready", () => {
             client.write("SUBSCRIBE TALLY\r\n")
+        })
+
+        client.on("timeout", () => {
+            console.error("Connection to vMix timed out")
+        })
+
+        client.on("error", error => {
+            console.error(error.name + ": " + error.message)
         })
 
         client.on('data', this.onData.bind(this))
 
-        client.on('close', function() {
-            console.log('Connection closed')
+        client.on('close', (hadError) => {
+            this.emitter.emit('mixer.disconnected')
+            console.log("Connection to vMix closed")
+
+            if (hadError) {
+                console.debug("Connection to vMix is reconnected after an error")
+                client.connect(this.port, this.ip)
+            }
         })
 
     }
     onData(data) {
         data.toString().replace(/[\r\n]*$/, "").split("\r\n").forEach(command => {
-            console.debug("Command: " + command)
+            console.debug("> " + command)
             if (command.startsWith("VERSION OK")) {
                 this.wasHelloReceived = true
             } else if (command.startsWith("SUBSCRIBE OK TALLY")) {
@@ -62,17 +89,28 @@ class VmixConnector {
             })
 
             this.emitter.emit("program.changed", programs, previews)
-
         }
     }
     disconnect() {
         this.wasHelloReceived = false
         this.wasSubcribeOkReceived = false
-        const promise = new Promise(resolve => 
-            this.client.end(() => {
-                console.log("Disconnected from vMix")
+        const promise = new Promise(resolve => {
+            if (this.client && ! this.client.destroyed) {
+                if (this.client.isConnected) {
+                    // if we are connected: try to be nice
+                    this.client.end(() => {
+                        console.log("Disconnected from vMix")
+                        resolve()
+                    })
+                } else {
+                    // if not: be rude
+                    this.client.destroy()
+                    resolve()
+                }
+            } else {
                 resolve()
-            }))
+            }
+        })
         this.client = null
         return promise
     }
