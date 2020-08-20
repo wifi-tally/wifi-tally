2d38004e3ea145ea1e03c6d5765b024f7a620a26
Christian Zenker <dev@xopn.de>
2020-08-20 19:56:57 +0200

    flatten unnecessary deep test depth

--- tally/spec/settings_spec.lua
+++ tally/spec/settings_spec.lua
@@ -1,4 +1,4 @@
-insulate("Settings parser", function()
+describe("Settings parser", function()
 
     local function mockSettings(fileName, additionalLines)
         -- mocks
@@ -30,179 +30,157 @@ insulate("Settings parser", function()
         }
     end
 
-    insulate("should parse all settings", function()
+    -- auto-insulate every test
+    local realIt = it
+    it = function(name, func)
+        insulate(function()
+            realIt(name, func)
+        end)
+    end
+
+    it("should parse all settings", function()
         mockSettings("settings-all.ini")
-        it(function()
-            require "src.my-settings"
+        require "src.my-settings"
 
-            assert.is_same("MyWifi", MySettings.staSsid())
-            assert.is_same("topsecret", MySettings.staPw())
-            assert.is_same("10.10.1.1", MySettings.hubIp())
-            assert.is_same(4242, MySettings.hubPort())
-            assert.is_same("Doe", MySettings.name())
-            assert.is_same("Tally-Doe", MySettings.hostName())
-        end)
+        assert.is_same("MyWifi", MySettings.staSsid())
+        assert.is_same("topsecret", MySettings.staPw())
+        assert.is_same("10.10.1.1", MySettings.hubIp())
+        assert.is_same(4242, MySettings.hubPort())
+        assert.is_same("Doe", MySettings.name())
+        assert.is_same("Tally-Doe", MySettings.hostName())
     end)
 
-    insulate("should parse a messy file", function()
+    it("should parse a messy file", function()
         mockSettings("settings-messy-file.ini")
-        it(function()
-            require "src.my-settings"
+        require "src.my-settings"
 
-            assert.is_same("MyWifi", MySettings.staSsid())
-            assert.is_same("topsecret", MySettings.staPw())
-            assert.is_same("10.10.1.1", MySettings.hubIp())
-            assert.is_same(4242, MySettings.hubPort())
-            assert.is_same("Doe", MySettings.name())
-            assert.is_same("Tally-Doe", MySettings.hostName())
-        end)
+        assert.is_same("MyWifi", MySettings.staSsid())
+        assert.is_same("topsecret", MySettings.staPw())
+        assert.is_same("10.10.1.1", MySettings.hubIp())
+        assert.is_same(4242, MySettings.hubPort())
+        assert.is_same("Doe", MySettings.name())
+        assert.is_same("Tally-Doe", MySettings.hostName())
     end)
 
-    insulate("should use defaults when optional values are", function()
+    it("should use defaults when optional values are", function()
         mockSettings("settings-partial.ini")
-        it(function()
-            require "src.my-settings"
+        require "src.my-settings"
 
-            assert.is_same(7411, MySettings.hubPort())
-            assert.is_same("bc614e", MySettings.name())
-            assert.is_same("Tally-bc614e", MySettings.hostName())
-        end)
+        assert.is_same(7411, MySettings.hubPort())
+        assert.is_same("bc614e", MySettings.name())
+        assert.is_same("Tally-bc614e", MySettings.hostName())
     end)
 
-    insulate("should accept tally name with spaces", function()
+    it("should accept tally name with spaces", function()
         mockSettings("settings-tallyname.ini")
-        it(function()
-            require "src.my-settings"
+        require "src.my-settings"
 
-            assert.is_same("John Doe", MySettings.name())
-            assert.is_same("Tally-John-Doe", MySettings.hostName())
-        end)
+        assert.is_same("John Doe", MySettings.name())
+        assert.is_same("Tally-John-Doe", MySettings.hostName())
     end)
 
-    insulate("should accept tally name with weired ascii chars", function()
+    it("should accept tally name with weired ascii chars", function()
         mockSettings("settings-tallyname2.ini")
-        it(function()
+        require "src.my-settings"
+
+        assert.is_true(MySettings.name():len() <= 32)
+        assert.is_true(MySettings.hostName():len() <= 32)
+        assert.is_same("Tally-this-is-a-really-messe", MySettings.hostName())
+    end)
+
+    describe("operator.type", function()
+        it("should be grb+ by default", function()
+            mockSettings("settings-all.ini")
             require "src.my-settings"
 
-            assert.is_true(MySettings.name():len() <= 32)
-            assert.is_true(MySettings.hostName():len() <= 32)
-            assert.is_same("Tally-this-is-a-really-messe", MySettings.hostName())
+            assert.is_same("grb+", MySettings.operatorType())
+        end)
+        it("should support grb+", function()
+            mockSettings("settings-all.ini", {
+                "operator.type=grb+"
+            })
+            require "src.my-settings"
+
+            assert.is_same("grb+", MySettings.operatorType())
+        end)
+        it("should support grb-", function()
+            mockSettings("settings-all.ini", {
+                "operator.type=grb-"
+            })
+            require "src.my-settings"
+
+            assert.is_same("grb-", MySettings.operatorType())
+        end)
+        it("should ignore case", function()
+            mockSettings("settings-all.ini", {
+                "operator.type=GrB-"
+            })
+            require "src.my-settings"
+
+            assert.is_same("grb-", MySettings.operatorType())
+        end)
+        it("should log a warning on invalid value and use the default", function()
+            mockSettings("settings-all.ini", {
+                "operator.type=invalid"
+            })
+
+            local warnings = {}
+            _G.MyLog = {
+                warning = function(warning) table.insert(warnings, warning) end
+            }
+
+            require "src.my-settings"
+
+            assert.is_same("Invalid operator.type \"invalid\"", warnings[1])
+            assert.is_same("grb+", MySettings.operatorType())
         end)
     end)
 
-    insulate("operator.type", function()
-        insulate("should be grb+ by default", function()
-            it(function()
-                mockSettings("settings-all.ini")
-                require "src.my-settings"
+    describe("stage.type", function()
+        it("should be grb+ by default", function()
+            mockSettings("settings-all.ini")
+            require "src.my-settings"
 
-                assert.is_same("grb+", MySettings.operatorType())
-            end)
+            assert.is_same("grb+", MySettings.stageType())
         end)
-        insulate("should support grb+", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "operator.type=grb+"
-                })
-                require "src.my-settings"
+        it("should support grb+", function()
+            mockSettings("settings-all.ini", {
+                "stage.type=grb+"
+            })
+            require "src.my-settings"
 
-                assert.is_same("grb+", MySettings.operatorType())
-            end)
+            assert.is_same("grb+", MySettings.stageType())
         end)
-        insulate("should support grb-", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "operator.type=grb-"
-                })
-                require "src.my-settings"
+        it("should support grb-", function()
+            mockSettings("settings-all.ini", {
+                "stage.type=grb-"
+            })
+            require "src.my-settings"
 
-                assert.is_same("grb-", MySettings.operatorType())
-            end)
+            assert.is_same("grb-", MySettings.stageType())
         end)
-        insulate("should ignore case", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "operator.type=GrB-"
-                })
-                require "src.my-settings"
+        it("should ignore case", function()
+            mockSettings("settings-all.ini", {
+                "stage.type=GrB-"
+            })
+            require "src.my-settings"
 
-                assert.is_same("grb-", MySettings.operatorType())
-            end)
+            assert.is_same("grb-", MySettings.stageType())
         end)
-        insulate("should log a warning on invalid value and use the default", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "operator.type=invalid"
-                })
+        it("should log a warning on invalid value and use the default", function()
+            mockSettings("settings-all.ini", {
+                "stage.type=invalid"
+            })
 
-                local warnings = {}
-                _G.MyLog = {
-                    warning = function(warning) table.insert(warnings, warning) end
-                }
+            local warnings = {}
+            _G.MyLog = {
+                warning = function(warning) table.insert(warnings, warning) end
+            }
 
-                require "src.my-settings"
+            require "src.my-settings"
 
-                assert.is_same("Invalid operator.type \"invalid\"", warnings[1])
-                assert.is_same("grb+", MySettings.operatorType())
-            end)
-        end)
-    end)
-
-    insulate("stage.type", function()
-        insulate("should be grb+ by default", function()
-            it(function()
-                mockSettings("settings-all.ini")
-                require "src.my-settings"
-
-                assert.is_same("grb+", MySettings.stageType())
-            end)
-        end)
-        insulate("should support grb+", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "stage.type=grb+"
-                })
-                require "src.my-settings"
-
-                assert.is_same("grb+", MySettings.stageType())
-            end)
-        end)
-        insulate("should support grb-", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "stage.type=grb-"
-                })
-                require "src.my-settings"
-
-                assert.is_same("grb-", MySettings.stageType())
-            end)
-        end)
-        insulate("should ignore case", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "stage.type=GrB-"
-                })
-                require "src.my-settings"
-
-                assert.is_same("grb-", MySettings.stageType())
-            end)
-        end)
-        insulate("should log a warning on invalid value and use the default", function()
-            it(function()
-                mockSettings("settings-all.ini", {
-                    "stage.type=invalid"
-                })
-
-                local warnings = {}
-                _G.MyLog = {
-                    warning = function(warning) table.insert(warnings, warning) end
-                }
-
-                require "src.my-settings"
-
-                assert.is_same("Invalid stage.type \"invalid\"", warnings[1])
-                assert.is_same("grb+", MySettings.stageType())
-            end)
+            assert.is_same("Invalid stage.type \"invalid\"", warnings[1])
+            assert.is_same("grb+", MySettings.stageType())
         end)
     end)
 
