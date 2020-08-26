de241eea039708adce9d6e16d63879d9c25002d9
Christian Zenker <dev@xopn.de>
2020-08-26 11:00:43 +0200

    replace zeit/sass with build-in scss support

--- hub/components/Layout.js
+++ hub/components/Layout.js
@@ -1,6 +1,4 @@
-
 import Link from 'next/link'
-import "../style/main.scss"
 
 const Layout = props => (
   <div>
---
+++ hub/pages/_app.js
@@ -0,0 +1,20 @@
+import '../style/main.scss'
+// import App from 'next/app'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp
